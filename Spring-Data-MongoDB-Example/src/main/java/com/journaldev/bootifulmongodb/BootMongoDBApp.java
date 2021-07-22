0414d46aa2eea00b5e8e3ff6bf37e5552def067e
yogesh147 <yogeshch147@gmail.com>
2021-07-22 20:39:36 +0530

    1 smart git commit

--- Spring-Data-MongoDB-Example/src/main/java/com/journaldev/bootifulmongodb/BootMongoDBApp.java
+++ Spring-Data-MongoDB-Example/src/main/java/com/journaldev/bootifulmongodb/BootMongoDBApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootMongoDBApp {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootMongoDBApp.class, args);
 	}
